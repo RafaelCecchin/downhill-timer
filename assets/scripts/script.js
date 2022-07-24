51ef3401180be00d863c71d76268352e2fc7e7b2
RafaelCecchin <contatorafaelcecchin@gmail.com>
2022-07-24 13:07:38 -0300

    feature: adicionar modal de teste

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,4 @@
 // Modal
-
 $('.close-modal').on('click', closeModal);
 
 function closeModal() {
@@ -16,4 +15,26 @@ function showModalInformation(title, message, okCallback = closeModal) {
     $('#modal-information').find('.information-title').text(title);
     $('#modal-information').find('.information-text').text(message);
     $('#modal-information').find('#btn-ok').off('click').on('click', okCallback);
+}
+function showModalTest(loadingMessage, testCallback, finishCallback = closeModal) {
+    $('#modal-test').attr('class', 'modal modal-test').addClass('show').addClass('status-loading');
+    $('#modal-test').find('.information-text').text(loadingMessage);
+    $('#modal-test').find('#btn-finalizar').off('click').on('click', finishCallback);
+
+    let testReturn = testCallback();
+    
+    if (testReturn.status) {
+        $('#modal-test').attr('class', 'modal modal-test show').addClass('status-finish');
+    } else {
+        $('#modal-test').attr('class', 'modal modal-test show').addClass('status-error');
+    }
+
+    $('#modal-test').find('.information-text').text(testReturn.message);
+}
+function testFunctionExample() {
+    let ret = new Object();
+    ret.status = 1; // 0 = Erro; 1 = ok;
+    ret.message = 'Ok';
+
+    return ret;
 }
