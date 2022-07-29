1fa83baee62954ff25f2b44a7d95bf9c6497e6cb
RafaelCecchin <contatorafaelcecchin@gmail.com>
2022-07-29 18:38:42 -0300

    refactor: criar controller separado para os campeonatos

---
+++ controllers/campeonato.js
@@ -0,0 +1,86 @@
+const models = require('../models');
+const Campeonato = models.Campeonato;
+
+exports.findAll = (req, res) => {
+    res.render('pages/campeonatos/index', {viewName: 'campeonatos'});
+};
+
+exports.new = (req, res) => {
+    res.render('pages/campeonatos/show', {viewName: 'campeonatos', formAction: 'create'});
+};
+
+exports.findOne = (req, res) => {
+    Campeonato.findByPk( req.params.id )
+        .then(data => {
+            if (data) {
+                res.render('pages/campeonatos/show', 
+                {
+                    viewName: 'campeonatos', 
+                    formAction: 'update',
+                    campeonato: data.dataValues
+                });
+            } else {
+                res.redirect('/campeonatos/create');
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                err.message
+            });
+        });
+};
+
+exports.create = (req, res) => {
+    const campeonato = { 
+        nome: req.body.nomeCampeonato 
+    }
+
+    Campeonato.create(campeonato)
+        .then(data => {
+            res.status(201).send(data);
+            })
+            .catch(err => {
+            res.status(500).send({
+                message:
+                err.message
+            });
+        });
+};
+
+exports.update = (req, res) => {
+    const campeonato = {
+        nome: req.body.nomeCampeonato
+    }
+
+    Campeonato.update(campeonato, {
+        where: { id: req.params.id }
+    })
+    .then(num => {
+        if (num == 1) {
+            res.status(204).send();
+        } else {
+            res.status(500).send({
+                message: `Campeonato não encontrado.`
+            });
+        }
+    })
+    .catch(err => {
+        res.status(500).send({
+            message:
+              err.message
+          });
+    });
+};
+
+exports.delete = (req, res) => {
+  
+};
+
+exports.deleteAll = (req, res) => {
+  
+};
+
+exports.findAllPublished = (req, res) => {
+  
+};
