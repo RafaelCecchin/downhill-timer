2d81069b000de68ee59e6897001ef8543f4d7593
RafaelCecchin <contatorafaelcecchin@gmail.com>
2022-08-01 15:23:20 -0300

    feature: adicionar generos

---
+++ seeders/20220801174756-generos.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('Generos', [
+      {
+        id: 1,
+        nome: 'Masculino',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        id: 2,
+        nome: 'Feminino',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    
+  }
+};
