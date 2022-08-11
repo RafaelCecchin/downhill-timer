cf30b1dc2a474213e79ce3836c4b6c4388813fe9
RafaelCecchin <contatorafaelcecchin@gmail.com>
2022-08-10 22:25:23 -0300

    refactor: vincular função de cancelar operação nas configurações

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -22,18 +22,29 @@ async function showModalTest(loadingMessage, testCallback, finishCallback = clos
     $('#modalTest').attr('class', 'modal modal-test').addClass('show').addClass('status-loading');
     $('#modalTest').find('.information-text').text(loadingMessage);
     $('#modalTest').find('#btnFinalizar').off('click').on('click', finishCallback).on('click', closeModal);
-    $('#modalTest').find('.close-modal').off('click').on('click', closeCallback).on('click', closeModal);
+    $('#modalTest').find('.close-modal').off('click').on('click', closeCallback).on('click', closeModal).on('click', function() {
+        if ($('#modalTest').hasClass('status-loading')) {
+            $('#modalTest').find('#btnCancelar').click();
+        }
+    });
+    $('#modalTest').find('#btnFechar').off('click').on('click', closeCallback).on('click', closeModal);
+    $('#modalTest').find('#btnCancelar').off('click').on('click', closeModal).on('click', function(event) {
+        $('#modalTest').addClass('canceled');
+        showModalInformation("Operação cancelada.");        
+    });
     $('#modalTest').find('#btnTentarNovamente').off('click').on('click', function() {
         showModalTest(loadingMessage, testCallback, finishCallback, closeCallback);
-    });
+    });    
     
     let testReturn = await testCallback();
     
-    if (testReturn.status) {
-        $('#modalTest').attr('class', 'modal modal-test show').addClass('status-finish');
-        $('#modalTest').find('.information-text').text(testReturn.message ?? "Teste finalizado com sucesso.");
-    } else {
-        $('#modalTest').attr('class', 'modal modal-test show').addClass('status-error');
-        $('#modalTest').find('.information-text').text(testReturn.message ?? "Ocorreu um erro ao realizar o teste.");
-    }    
+    if (!$('#modalTest').hasClass('canceled')) {
+        if (testReturn.status) {
+            $('#modalTest').attr('class', 'modal modal-test show').addClass('status-finish');
+            $('#modalTest').find('.information-text').text(testReturn.message ?? "Teste finalizado com sucesso.");
+        } else {
+            $('#modalTest').attr('class', 'modal modal-test show').addClass('status-error');
+            $('#modalTest').find('.information-text').text(testReturn.message ?? "Ocorreu um erro ao realizar o teste.");
+        }
+    }        
 }
