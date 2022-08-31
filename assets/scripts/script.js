9e289ea58902b2dde0befa1d30826d6a5b3fd89a
RafaelCecchin <contatorafaelcecchin@gmail.com>
2022-08-31 19:50:38 -0300

    refactor: remover partial do modal de teste

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -23,36 +23,6 @@ function showModalInformation(message, okCallback = closeModal, closeCallback = 
     $('#modalInformation').find('#btnOk').off('click').on('click', okCallback).on('click', closeModal);
     $('#modalInformation').find('.close-modal').off('click').on('click', closeCallback).on('click', closeModal);
 }
-async function showModalTest(loadingMessage, testCallback, finishCallback = closeModal, closeCallback = closeModal) {
-    $('#modalTest').attr('class', 'modal modal-test').addClass('show').addClass('status-loading');
-    $('#modalTest').find('.information-text').text(loadingMessage);
-    $('#modalTest').find('#btnFinalizar').off('click').on('click', finishCallback).on('click', closeModal);
-    $('#modalTest').find('.close-modal').off('click').on('click', closeCallback).on('click', closeModal).on('click', function() {
-        if ($('#modalTest').hasClass('status-loading')) {
-            $('#modalTest').find('#btnCancelar').click();
-        }
-    });
-    $('#modalTest').find('#btnFechar').off('click').on('click', closeCallback).on('click', closeModal);
-    $('#modalTest').find('#btnCancelar').off('click').on('click', closeModal).on('click', function(event) {
-        $('#modalTest').addClass('canceled');
-        showModalInformation("Operação cancelada.");        
-    });
-    $('#modalTest').find('#btnTentarNovamente').off('click').on('click', function() {
-        showModalTest(loadingMessage, testCallback, finishCallback, closeCallback);
-    });    
-    
-    let testReturn = await testCallback();
-    
-    if (!$('#modalTest').hasClass('canceled')) {
-        if (testReturn.status) {
-            $('#modalTest').attr('class', 'modal modal-test show').addClass('status-finish');
-            $('#modalTest').find('.information-text').text(testReturn.message ?? "Teste finalizado com sucesso.");
-        } else {
-            $('#modalTest').attr('class', 'modal modal-test show').addClass('status-error');
-            $('#modalTest').find('.information-text').text(testReturn.message ?? "Ocorreu um erro ao realizar o teste.");
-        }
-    }        
-}
 
 // Helper
 
