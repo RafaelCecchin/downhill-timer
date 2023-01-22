42706f09550ac293d0ab95c30c9f133623c612a6
Rafael Cecchin <contatorafaelcecchin@gmail.com>
2023-01-22 10:47:39 -0300

    fix: informar configuração inicial correta para os dispositivos

--- arduino/main/setupDeviceCentral.ino
+++ arduino/main/setupDeviceCentral.ino
@@ -1,5 +1,4 @@
 void setupDeviceCentral() {
-  setupSD();
-  setupRTC();
   setupLoRa();
+  setupEspNow();
 }
--- arduino/main/setupDeviceLargada.ino
+++ arduino/main/setupDeviceLargada.ino
@@ -1,4 +1,5 @@
 void setupDeviceLargada() {
-  setupEspNow();
+  setupSD();
+  setupRTC();
   setupLoRa();
 }
